7dd00c75229d3bb615052cd4a89fd28efa76bbdd
William Yu <wilyu@ebay.com>
2017-10-10 16:22:03 -0700

    add a skeleton class fo rtesting purposes

---
+++ src/test/java/com/stubhub/skakria/dhash/DownloadImageTest.java
@@ -0,0 +1,18 @@
+package com.stubhub.skakria.dhash;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DownloadImageTest {
+
+    @BeforeMethod
+    public void setup() {
+
+    }
+
+    @Test
+    public void testFindDuplicates() {
+        // TODO: fill in unit tests here.
+    }
+
+}
