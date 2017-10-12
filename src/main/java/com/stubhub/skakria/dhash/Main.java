a47c0e71dd8ffe6dca3789491d3b9c4a7968ce47
William Yu <wilyu@ebay.com>
2017-10-11 19:01:12 -0700

    modify Main to make use of command line arguments

--- src/main/java/com/stubhub/skakria/dhash/Main.java
+++ src/main/java/com/stubhub/skakria/dhash/Main.java
@@ -1,15 +1,28 @@
 package com.stubhub.skakria.dhash;
 
-import com.stubhub.skakria.dhash.DownloadImage;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Please enter correct values; usage for this " +
+                    "program is as follows: java -jar <this jar> " +
+                    "<absolute_path_to_url_file> <absolute_path_to_output_dir");
+            System.exit(1);
+        }
+
+        String urlsFilePath = args[0];
+        String outputDirPath = args[1];
+
         String imageUrl = "https://pixel.nymag.com/imgs/fashion/daily/2016/02/29/29-liu-wen.w245.h368.2x.jpg";
         String filePath = "/Users/skakria/Projects/images/Urls";
 
-        DownloadImage.findDuplicates(imageUrl, filePath); // returns a filePath to the
-        // duplicate image
+        try {
+            DownloadImage.saveAndFindDuplicates(imageUrl, filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
